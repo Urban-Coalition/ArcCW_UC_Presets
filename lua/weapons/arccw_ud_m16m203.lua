f04c0329ed7764a4cab18baa8d14df1af7c0ed96
Sninctbur <hedpub@protonmail.com>
2022-12-23 15:11:43 -0600

    Create arccw_ud_m16m203.lua

---
+++ lua/weapons/arccw_ud_m16m203.lua
@@ -0,0 +1,15 @@
+if !file.Exists("arccw/shared/languages/ud_en.lua","LUA") then return end
+
+include("weapons/arccw_ud_m16.lua")
+
+SWEP.Category = "ArcCW - UC Presets"
+
+SWEP.PrintName = "RAYCAR-0 Grenadier"
+SWEP.TrueName = "M16A2 Grenadier"
+if GetConVar("arccw_truenames"):GetBool() then
+    SWEP.PrintName = SWEP.TrueName
+end
+
+SWEP.Attachments[3].Installed = "ud_m16_hg_heat"
+SWEP.Attachments[16].Installed = "ud_m16_charm_ch"
+SWEP.Attachments[18].Installed = "uc_ubgl_m203"-- UBGLs go in the funny hidden slot, not the UB slot
